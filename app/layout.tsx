34afb53632f3e26fd6a249ce4336f3aa5b8662a9
João Pedro <joaopedro.b.c@hotmail.com>
2026-04-03 17:11:41 -0300

    Tentando arrumar a transparencia da barra da url 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,6 @@ export const metadata: Metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  viewportFit: "cover",
-  AppleMobileWebAppCapable: "yes",
-  AppleMobileWebAppStatusBarStyle: "default",
 };
 
 const poppins = Poppins({
